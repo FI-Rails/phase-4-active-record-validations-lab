56a2da12468c8487fb0609ef49a829520c2a1fc1
Pauline-momanyi <paulinemomanyi55@gmail.com>
2022-07-21 10:51:16 +0300

    commit tree 66a21028e065ff753a8ca2266d0c499e31096f0d

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,20 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: {minimum: 250}
+    validates :summary, length: {maximum: 250}
+    validates :category, inclusion: {in: %w(Fiction Non-Fiction)}
+    # validates :category, inclusion: { in: ['Fiction', 'Non-Fiction'] }
+    validate :title_is_clickbait
+
+    def title_is_clickbait
+        pattern = [
+            /Won't Believe/i,
+            /Secret/i,
+            /top \d/i,
+            /Guess/i
+         ]
+        if pattern.none? {|pat| pat.match title}
+            errors.add(:title, "must be clickbait")
+        end
+    end
 end
